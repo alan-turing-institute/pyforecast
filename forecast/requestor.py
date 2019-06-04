5768fcdfba3b0fc79db36aba3f13057a0543b09f
Jack Roberts <jroberts@turing.ac.uk>
2019-06-04 18:07:53 +0100

    remove cache print statement

--- forecast/requestor.py
+++ forecast/requestor.py
@@ -27,7 +27,8 @@ class Requestor:
         start = timer()
         r = requests.get("{}/{}".format(self._base_url, endpoint), headers=self._headers)
         end = timer()
-        print("Enpoint: {0} | Time: {1} | Cache: {2}".format(endpoint, end - start, r.from_cache, ))
+        # print("Enpoint: {0} | Time: {1} | Cache: {2}".format(endpoint, end - start, r.from_cache, ))
+        print("Enpoint: {0} | Time: {1} ".format(endpoint, end - start))
         return r.json()
 
     #@staticmethod
